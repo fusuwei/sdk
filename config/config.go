52465f652d266e7c27481c67f5710f92f7776119
付苏维 <fusuwei@shining3d.com>
2022-10-19 14:42:07 +0800

    config增加默认值函数

--- config/config.go
+++ config/config.go
@@ -51,12 +51,33 @@ func (c Config) Get(key string) interface{} {
 	return c.Viper.Get(key)
 }
 
+func (c Config) GetDefault(key string, item interface{}) interface{} {
+	if v := c.Viper.Get(key); v != nil {
+		return v
+	}
+	return item
+}
+
 func (c Config) GetString(key string) string {
 	return c.Viper.GetString(key)
 }
 
+func (c Config) GetDefaultString(key string, item string) string {
+	if v := c.Viper.GetString(key); v != "" {
+		return v
+	}
+	return item
+}
+
 func (c Config) GetInt(key string) int {
 	return c.Viper.GetInt(key)
+}
+
+func (c Config) GetDefaultInt(key string, item int) int {
+	if v := c.Viper.GetInt(key); v != 0 {
+		return v
+	}
+	return item
 }
 
 func (c Config) GetBool(key string) bool {
